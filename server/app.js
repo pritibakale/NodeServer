98c84b6a90a650d5ee5cf174f3accb98c6ef5d75
Priti Bakale <bakalepriti1@gmail.com>
2022-05-05 07:35:40 +0530

    Added edit api

--- server/app.js
+++ server/app.js
@@ -77,9 +77,23 @@ app.post('/api/posts',(req, res)=>{
 
 });
 
+app.put('/api/posts/:id', (req, res, next)=>{
+    const post = new Post({
+        _id: req.body.id,
+        title: req.body.title,
+        content: req.body.content
+    });
+    Post.updateOne({_id: req.params.id}, post).then(result =>{
+        console.log(result);
+        res.status(200).json({message: "updated successfully"});
+    });
+});
+
 app.delete('/api/posts/:id', (req, res)=>{
-    console.log("Post deleted Successfully");
-    res.status(200).json({message: "Post deleted successfully!"})
+    Post.deleteOne({_id: req.params.id}).then(result =>{
+        console.log(result);
+        res.status(200).json({message: "Post Delete!"});
+    });
 });
 
 module.exports = app;
